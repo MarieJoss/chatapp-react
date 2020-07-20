e5c91becb70cd206ff76cd1e6b10564b489e5e67
Marie Joss <mjosselin6@gmail.com>
2020-07-20 18:53:48 +0200

    netlify problem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from "react";
 import "./App.css";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Route, Switch, BrowserRouter } from "react-router-dom";
 
 import Join from "./components/Join/Join";
 import Chat from "./components/Chat/Chat";
 
 const App = () => (
-  <Router>
+  <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Join} />
       <Route path="/chat" component={Chat} />
     </Switch>
-  </Router>
+  </BrowserRouter>
 );
 
 export default App;
