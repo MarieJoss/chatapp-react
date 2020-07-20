3159c54165bb676696d51f8ab7ee7f8192ad59f1
Marie Joss <mjosselin6@gmail.com>
2020-07-20 18:56:40 +0200

    probleme netlify browserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import { BrowserRouter, Route, Switch, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Join from "./components/Join/Join";
 import Chat from "./components/Chat/Chat";
